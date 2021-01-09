07630ba92d7e520a100143eadbfde78acf65d8ac
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2021-01-09 12:26:27 +0300

    filter is fired several times for each line for some reason - don't bother reprocessing same line

--- SR13-AntiSpam.lua
+++ SR13-AntiSpam.lua
@@ -10,15 +10,26 @@ local patterns_boost = a_env.patterns_boost
 local cleanup_pattern = a_env.cleanup_pattern
 local cleanup_replace = a_env.cleanup_replace
 
-local function msg_channel_filter(self, event, msg, author, _, _, _, specialFlag, zoneChannelID)
+local prev_lineID, prev_result
+local function msg_channel_filter(self, event, msg, author, _, _, _, specialFlag, zoneChannelID, _, _, _, lineID)
    if not zoneChannelID then return end
+
+   -- For some reason chat system calls filter several times on each line
+   -- Don't bother reprocessing it and just return same result
+   if lineID == prev_lineID then return prev_result end
+   prev_lineID = lineID
+   prev_result = nil
+
    if specialFlag == "GM" or specialFlag == "DEV" then return end
 
-   local known = known_spammers[author] or 0
+   -- All checks above are trivial, withou side-effects and return nil,
+   -- everything below this line MUST update prev_result.
 
+   local known = known_spammers[author] or 0
    if known > 5 then
       -- print("known", msg)
-      return true
+      prev_result = true
+      return prev_result
    end
 
    msg = strgsub(msg, cleanup_pattern, cleanup_replace)
@@ -27,7 +38,8 @@ local function msg_channel_filter(self, event, msg, author, _, _, _, specialFlag
       if strmatch(msg, pattern) then
          -- print("spam", msg)
          known_spammers[author] = known + 1
-         return true
+         prev_result = true
+         return prev_result
       end
    end
 end
