51eb101570c82f3e7286796cfbc85f5fd603a42c
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2021-01-13 13:39:20 +0300

    revert order of lines in dump

--- dump.lua
+++ dump.lua
@@ -6,7 +6,8 @@ local sv = "SVPC_SR13_AnitSpam_dump"
 function cf1dump(frame)
     if not frame then frame = ChatFrame1 end
     local dump = {}
-    for idx, entry in ipairs(frame.visibleLines) do
+    for idx = #frame.visibleLines, 1, -1 do
+      local entry = frame.visibleLines[idx]
       dump[idx] = entry.messageInfo.message
     end
     _G[sv] = dump
