37c8fcb9147dd6439c022a1992f6947b8e30b816
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2021-01-13 13:40:39 +0300

    noop - whitespace fix

--- dump.lua
+++ dump.lua
@@ -4,11 +4,11 @@ if not a_env.load_this then return end
 local sv = "SVPC_SR13_AnitSpam_dump"
 
 function cf1dump(frame)
-    if not frame then frame = ChatFrame1 end
-    local dump = {}
-    for idx = #frame.visibleLines, 1, -1 do
+   if not frame then frame = ChatFrame1 end
+   local dump = {}
+   for idx = #frame.visibleLines, 1, -1 do
       local entry = frame.visibleLines[idx]
       dump[idx] = entry.messageInfo.message
-    end
-    _G[sv] = dump
+   end
+   _G[sv] = dump
 end
