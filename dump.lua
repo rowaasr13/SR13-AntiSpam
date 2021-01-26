fb14976e970727a04f9df5ccfaa7aff9f6507b9d
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2021-01-26 17:21:14 +0300

    add longer explanation messages if dump has failed; .messageInfo could be missing on first lines when you've just logged in the game - check and skip those

--- dump.lua
+++ dump.lua
@@ -4,11 +4,27 @@ if not a_env.load_this then return end
 local sv = "SVPC_SR13_AnitSpam_dump"
 
 local function ChatFrameDump(frame)
-   if not frame then frame = ChatFrame1 end
+   if frame then
+   else
+      if not ChatFrame1 then
+         _G[sv] = "ChatFrame1 not found. It is likely that you use custom chat UI addon. Please send addon's name if you want it added to reporting tool."
+         return
+      end
+      frame = ChatFrame1
+   end
+
+   if not frame.visibleLines then
+      local frame_name = frame.GetName and frame:GetName() or ':GetName() absent'
+      _G[sv] = ("No .visibleLines data on frame %q. It is likely that you use custom chat UI addon or mistyped frame name. Please send addon's name if you want it added to reporting tool."):format(frame_name)
+      return
+   end
+
    local dump = {}
    for idx = #frame.visibleLines, 1, -1 do
       local entry = frame.visibleLines[idx]
-      dump[idx] = entry.messageInfo.message
+      if entry.messageInfo and entry.messageInfo.message then
+         dump[idx] = entry.messageInfo.message
+      end
    end
    _G[sv] = dump
 end
