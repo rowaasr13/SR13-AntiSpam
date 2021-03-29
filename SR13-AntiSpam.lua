056d5b5d6c406934c1470931ed09409202eacd82
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2021-03-29 23:34:26 +0300

    verification/profiling concluded: remove old engine, its data file and verification/profiling harness, only new engine is active now

--- SR13-AntiSpam.lua
+++ SR13-AntiSpam.lua
@@ -43,54 +43,24 @@ local function msg_channel_filter(self, event, msg, author, _, _, _, specialFlag
 
    msg = strgsub(msg, cleanup_pattern, cleanup_replace)
 
-   local profile_f
-   local res_f = (function() local pattren_loop_start = debugprofilestop()
-   local pattren_loop_end
-   for idx = 1, #patterns_boost do
-      local pattern = patterns_boost[idx]
-      if strfind(msg, pattern) then
-         local highlight_spam = options.highlight_spam
-         if highlight_spam then
-            if highlight_spam == "author" then
-               print("spam", BLUE_FONT_COLOR:GenerateHexColorMarkup() .. msg, author)
-            else
-               print("spam", BLUE_FONT_COLOR:GenerateHexColorMarkup() .. msg)
-            end
+   local res
+   do
+      res = is_imptree_boost(msg)
+   end
+
+   if res then
+      local highlight_spam = options.highlight_spam
+      if highlight_spam then
+         if highlight_spam == "author" then
+            print("spam", BLUE_FONT_COLOR:GenerateHexColorMarkup() .. msg, author)
+         else
+            print("spam", BLUE_FONT_COLOR:GenerateHexColorMarkup() .. msg)
          end
-         known_spammers[author] = known + 1
-         prev_result = true
-         pattren_loop_end = debugprofilestop()
-         profile_f = pattren_loop_end - pattren_loop_start
-         return prev_result
       end
+      known_spammers[author] = known + 1
+      prev_result = true
+      return prev_result
    end
-   pattren_loop_end = debugprofilestop()
-   profile_f = pattren_loop_end - pattren_loop_start
-   end)()
-
-   local profile_t
-   local res_t = (function() local pattren_loop_start = debugprofilestop()
-   local pattren_loop_end
-
-   local res = is_imptree_boost(msg)
-   pattren_loop_end = debugprofilestop()
-   profile_t = pattren_loop_end - pattren_loop_start
-   return res
-
-   end)()
-
-   if a_env.is_devel then
-      local profile = SVPC_SR13_AnitSpam_profile
-      if not profile then profile = {} SVPC_SR13_AnitSpam_profile = profile end
-      profile[#profile + 1] = { res_f = res_f, profile_f = profile_f, res_t = res_t, profile_t = profile_t }
-      print(
-         "f:" .. (res_f and "O" or "x") .. " " .. profile_f .. "ms " ..
-         "t:" .. (res_t and "O" or "x") .. " " .. profile_t .. "ms ",
-         ((not not res_t) ~= (not not res_f) and msg or '')
-      )
-   end
-
-   return res_f
 end
 
 ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", msg_channel_filter)
